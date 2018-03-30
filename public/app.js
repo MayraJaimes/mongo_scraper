c06ecf6478fd29db598d694705080841b7373ce1
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-30 17:15:09 -0500

    able to get information displayed

---
+++ public/app.js
@@ -0,0 +1,23 @@
+function displayResults() {
+  $("#articles").empty();
+  $.getJSON("/all", function(data) {
+    for (var i = 0; i < data.length; i++) {
+      $("#articles").append("<div class='data-entry' data-id=" + data[i]._id + "> <a href='" + data[i].link + "'> <h4>" + data[i].title+ "</h4></a>  <button class='btn' class='save-article'> Save </button> </div>");
+    }
+  });
+}
+
+displayResults();
+
+
+// $("#scrape-articles").on("click", function() {
+//   $.getJSON("/all", function(data) {
+//     displayResults(data);
+//   });
+// });
+
+// $("#save-article").on("click", function() {
+//   $.getJSON("/saved", function(data) {
+//     displayResults(data);
+//   });
+// });
--- server.js
+++ server.js
@@ -2,19 +2,30 @@ const express = require("express");
 const mongojs = require("mongojs");
 const request = require("request");
 const cheerio = require("cheerio");
+const bodyParser = require("body-parser");
+
 
 var app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(express.static("public"));
 
 var databaseUrl = "scraper";
 var collections = ["scrapedData"];
 
 var db = mongojs(databaseUrl, collections);
+
 db.on("error", function(error) {
   console.log("Database Error:", error);
 });
 
 app.get("/", function(req, res) {
-  res.send("Front Index Page");
+  res.send(index.html);
 });
 
 app.get("/all", function(req, res) {
@@ -53,9 +64,21 @@ app.get("/scrape", function(req, res) {
       }
     });
   });
-  res.send("Scrape Complete");
+  res.send("Scraped");
 });
 
+app.get("/clearall", function(req, res) {
+  db.scrapedData.remove({}, function(error, response) {
+    if (error) {
+      console.log(error);
+      res.send(error);
+    }
+    else {
+      console.log(response);
+      res.send(response);
+    }
+  });
+});
 
 app.listen(3000, function() {
   console.log("App running on port 3000!");
